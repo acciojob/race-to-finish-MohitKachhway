2808916d4f6d731895530df7691437aedcf4625f
Mohit kachhaway <130671896+MohitKachhway@users.noreply.github.com>
2023-08-25 08:43:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-window.promises = [];
+let prom1 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("mohit");
+	},2000)
+});
+let prom2 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("Ram");
+	},1000)
+})
+let prom3 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		reject("rohit");
+	},1000)
+})
+	window.promises = [prom1,prom2,prom3];
+let x = Promise.any(promises);
+x.then((data)=>{
+	output.textContent = data;
+})
+let output = document.getElementById("output");
+
 
 // Do not change the code above this
 // add your promises to the array `promises`
